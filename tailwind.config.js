65f7ef9065073c316f726c7f626eacb9c5ba75fa
Ramy Jaber <nickobabychoppa@gmail.com>
2024-08-25 12:30:11 +0300

    re-add the cdn links

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,12 @@
 module.exports = {
   content: ["./**/*.html", "./**/*.templ", "./**/*.go"],
   safelist: [],
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    // Optional: DaisyUI configuration options
+    themes: ["light", "dark"], // You can customize the themes here
+  },
 };
