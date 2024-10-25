11f2bac595fb96819974ab462cc8f463be93275f
Ramy <nickobabychoppa@gmail.com>
2024-10-25 16:34:03 +0300

    random stuff from master

--- models/shop.models.go
+++ models/shop.models.go
@@ -3,20 +3,24 @@ package models
 import "time"
 
 type Shop struct {
-	ID                uint      `form:"id" gorm:"primaryKey"`
-	Name              string    `form:"name"`
-	OwnerID           uint      `form:"owner_id"`
-	Owner             User      `form:"owner" gorm:"foreignKey:OwnerID"`
-	Address           string    `form:"address"`
-	Image             string    `form:"image"`
-	Workers           []User    `form:"workers" gorm:"foreignKey:ShopID"`
-	Status            string    `form:"status" gorm:"default:ACTIVE"`
-	TotalIncome       float64   `form:"total_income"`
-	TotalExpenses     float64   `form:"total_expenses"`
-	TotalClients      int       `form:"total_clients"`
-	TotalAppointments int       `form:"total_appointments"`
-	ProjectName       string    `form:"project_name"`
-	TopClients        []User    `form:"top_clients"`
-	CreatedAt         time.Time `form:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt         time.Time `form:"updated_at" gorm:"autoUpdateTime"`
+	ID                uint       `form:"id" gorm:"primaryKey"`
+	Name              string     `form:"name"`
+	OwnerID           uint       `form:"owner_id"`
+	Owner             User       `form:"owner" gorm:"foreignKey:OwnerID"`
+	Address           string     `form:"address"`
+	Image             string     `form:"image"`
+	Workers           []User     `form:"workers" gorm:"foreignKey:ShopID"`
+	Status            string     `form:"status" gorm:"default:ACTIVE"`
+	MembershipID      uint       `form:"membership_id"`
+	Membership        Membership `form:"membership" gorm:"foreignKey:MembershipID"`
+	NextBillingDate   *time.Time `form:"next_billing_date"`
+	SendWp            bool       `form:"send_wp"`
+	TotalIncome       float64    `form:"total_income"`
+	TotalExpenses     float64    `form:"total_expenses"`
+	TotalClients      int        `form:"total_clients"`
+	TotalAppointments int        `form:"total_appointments"`
+	ProjectName       string     `form:"project_name"`
+	TopClients        []User     `form:"top_clients"`
+	CreatedAt         time.Time  `form:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt         time.Time  `form:"updated_at" gorm:"autoUpdateTime"`
 }
